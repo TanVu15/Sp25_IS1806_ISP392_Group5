4e3135ece025aa77e23b82e46f5ecb93c3ab2705
Viet <viethvhe186511@fpt.edu.vn>
2025-02-18 18:52:38 +0700

    update db con text, debt

--- ISP/src/java/dal/DBContext.java
+++ ISP/src/java/dal/DBContext.java
@@ -26,10 +26,10 @@ public class DBContext {
      }
 
     private final String serverName = "localhost";
-    private final String dbName = "ISP1";
+    private final String dbName = "ISP";
     private final String portNumber = "1433";
     private final String userID = "sa";
-    private final String password = "ngoc";
+    private final String password = "123";
 
     
 }
